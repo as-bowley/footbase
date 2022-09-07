72069e77e7cec328a8be8805ec3eff2ccb38c1bc
as-bowley <as.bowley@gmail.com>
2022-09-07 14:30:05 +0200

    update news api

--- src/Components/Home/News.js
+++ src/Components/Home/News.js
@@ -4,24 +4,25 @@ const News = ({ headlines, darkMode }) => {
   return (
     <div className="home__headlines">
       <h2>Latest Headlines</h2>
-      {headlines.map((headline, i) => {
-        return (
-          <a
-            className={`home__headline ${darkMode ? "dark" : null}`}
-            key={i}
-            href={headline.link}
-            target="_blank"
-            rel="noreferrer"
-            style={darkMode ? { color: "#fff" } : null}
-          >
-            <img src={headline.media} alt="" width={"150px"} />
-            <div>
-              <h3>{headline.title}</h3>
-              <span>{headline.clean_url}</span>
-            </div>
-          </a>
-        );
-      })}
+      {headlines !== undefined &&
+        headlines.map((headline, i) => {
+          return (
+            <a
+              className={`home__headline ${darkMode ? "dark" : null}`}
+              key={i}
+              href={headline.link}
+              target="_blank"
+              rel="noreferrer"
+              style={darkMode ? { color: "#fff" } : null}
+            >
+              <img src={headline.media} alt="" width={"150px"} />
+              <div>
+                <h3>{headline.title}</h3>
+                <span>{headline.clean_url}</span>
+              </div>
+            </a>
+          );
+        })}
     </div>
   );
 };
